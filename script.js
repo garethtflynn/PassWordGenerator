410f8eb5419208129e55132ab7ed25dc6067c114
Gareth Flynn <gareth.t.flynn@gmail.com>
2022-07-25 18:18:01 -0400

    fixed jacascript

--- script.js
+++ script.js
@@ -1,69 +1,66 @@
  // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz'
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+var numbers = '0123456789'
+var symbols = '!"#$%&()*+,-./:;<=>?@[\]^_`{|}~' 
 
-var symbols = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', " \ ", ']', '^', '_', '`', '{', '|', '}', '`']
-
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-
-function generatePassword() {
-  // - prompt user for password specs:
-  //    a. password length 8-128 
-  //    b. prompt user for uppercase, lowercase, numbers, and special characters
-
-  var userDecision = window.prompt('How long would you like your password to be? (Choose between 8-128 characters)');
+function generatePassword(length, includeUC,includeLC,includeNum,includeSym) {
+  //  prompt user for password specs:
+  //  password length between 8-128 characters 
+  var userChoiceLength = window.prompt('How long would you like your password to be? (Choose between 8-128 characters)');
  
-  var userDecisionLength = parseInt(userDecision);
-
-  console.log(userDecisionLength);
+  length = parseInt(userChoiceLength);
   
-  if (userDecision <  8 || userDecision > 128){
+  if (length <  8 || length > 128){
     window.alert('Oops! Got to be between 8-128 characters')
     return
   }
 
-  var userIncludeUC = window.confirm('Would you like to include uppercase letters?');
-  var userIncludeLC = window.confirm ('Would you like to include lowercase letters?');
-  var userIncludeNum = window.confirm('Would you like to include numbers?');
-  var userIncludeSym = window.confirm('Would you like to include symbols?');
+  //  prompt user for uppercase, lowercase, numbers, and special characters
+  var includeUC = window.confirm('Would you like to include uppercase letters?');
+  var includeLC = window.confirm ('Would you like to include lowercase letters?');
+  var includeNum = window.confirm('Would you like to include numbers?');
+  var includeSym = window.confirm('Would you like to include symbols?');
 
-  var userSpecs = []
- 
   // - validate the input
-  if (userIncludeUC){
-    userSpecs.push(upperCase);
+  var userSpecs = ''
+
+  if (includeUC){
+    userSpecs+=(upperCase);
   }
 
-  if (userIncludeLC){
-    userSpecs.push(lowerCase);
+  if (includeLC){
+    userSpecs+=(lowerCase);
   }
 
-  if(userIncludeNum){
-    userSpecs.push(numbers);
+  if(includeNum){
+    userSpecs+=(numbers);
   }
 
-  if (userIncludeSym){
-    userSpecs.push(symbols); 
-  }
-  console.log(userSpecs);  
-  
+  if (includeSym){
+    userSpecs+=(symbols); 
+  } 
   if (userSpecs == 0) {
     window.alert ('Oops! you must choose at least one character type.')
   };
 
+  console.log(userSpecs)
+
   // - generate password
   var userPassword = [];
-  for (var i = 0; i < userDecisionLength.length; i++) {
-    var randomIndex = Math.floor(Math.random() * userSpecs.length);
-    userPassword += userSpecs[randomIndex]; 
-    console.log(userPassword )
+  for (var i = 0; i <length; i++) { 
+    var randomPw = Math.floor(Math.random() * userSpecs.length);
+    console.log(userSpecs[randomPw]) 
+    userPassword += userSpecs[randomPw]; 
+
+    // console.log(randomPw)
   }
 
 
   // display password 
-  return userPassword    
+  return userPassword;  
 }
 
 
